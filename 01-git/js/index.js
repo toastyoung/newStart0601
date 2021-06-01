2e0a53f6f97457de0927dbf1b3dd71a606089c18
toastyoung <979413572@qq.com>
2021-06-01 20:29:15 +0800

    练习代码

---
+++ 01-git/js/index.js
@@ -0,0 +1,16 @@
+console.log(123);
+
+const server = http.createServer(() => {
+    console.log(123);
+})
+
+<<<<<<< HEAD
+console.log(1111);
+=======
+server.listen(5000, () => {})
+console.log(3333);
+console.log(3333);
+console.log(3333);
+console.log(3333);
+console.log(3333);
+>>>>>>> dev
